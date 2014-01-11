4ce22e30fc8363484acbf6ca285302e539314aa9
jakenotjacob <jake.campbell91@gmail.com>
2014-01-11 13:22:42 -0700

    Initial commit. Rails application generated.

---
+++ ServiceDesk/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ServiceDesk::Application.config.secret_key_base = '9655e7a6eb4c6a080bd1049f44da56db54c649c932f027e4530c5b393ae3d89902b965c4fdbb1a7300838a6b0c2f9b015bf186141a97899d15b2a470395012b7'
---
+++ ServiceDesk/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ServiceDesk::Application.config.session_store :cookie_store, key: '_ServiceDesk_session'
