acbc7c61a001e6d36f09e2d45b487ad47be13c4e
jakenotjacob <jake.campbell91@gmail.com>
2014-01-11 19:18:36 -0700

    Customer Model: generated and migrated.

--- ServiceDesk/db/schema.rb
+++ ServiceDesk/db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140112021404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone_number"
+    t.string   "address"
+    t.integer  "zip_code"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
