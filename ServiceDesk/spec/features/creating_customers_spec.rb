ee74e69b17bc00ceecdbccb2833e7d5672892d3c
jakenotjacob <jake.campbell91@gmail.com>
2014-01-11 19:43:45 -0700

    :q!":q:q	:q

--- ServiceDesk/spec/features/creating_customers_spec.rb
+++ ServiceDesk/spec/features/creating_customers_spec.rb
@@ -12,7 +12,7 @@ describe "Creating Customers" do
       fill_in "First Name", with: "John"
       fill_in "Last Name", with: "Doe"
       fill_in "Phone Number", with: "3031112222"
-      fill_in "Street", with: "1234 S. Testing St."
+      fill_in "Address", with: "1234 S. Testing St."
       fill_in "Zip Code", with: "12345"
 
       click_button "Create"
