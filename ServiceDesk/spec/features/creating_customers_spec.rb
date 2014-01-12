b1bd2e5f62fe42ee8f2b7d9d969f4dca45cb5d6d
jakenotjacob <jake.campbell91@gmail.com>
2014-01-11 18:59:51 -0700

    Customer: Controller generated, resource added, spec modified, and basic views created.

--- ServiceDesk/spec/features/creating_customers_spec.rb
+++ ServiceDesk/spec/features/creating_customers_spec.rb
@@ -6,12 +6,14 @@ describe "Creating Customers" do
 
     it "displays the New Customer form" do
       visit '/'
+
+      click_link "New Customer"
+
       fill_in "First Name", with: "John"
       fill_in "Last Name", with: "Doe"
       fill_in "Phone Number", with: "3031112222"
       fill_in "Street", with: "1234 S. Testing St."
       fill_in "Zip Code", with: "12345"
-      fill_in "State", with: "CO"
 
       click_button "Create"
 
