ab210e0afdb09c87cfdfb7d4df6f668c235a49e0
jakenotjacob <jake.campbell91@gmail.com>
2014-01-14 07:20:42 -0700

    Modified initial Viewing Customers spec.

--- ServiceDesk/app/models/customer.rb
+++ ServiceDesk/app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ActiveRecord::Base
   validates :first_name, :last_name, :phone_number, :address, :zip_code, 
             presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- ServiceDesk/spec/features/customers/viewing_customers_spec.rb
+++ ServiceDesk/spec/features/customers/viewing_customers_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
-describe "Viewing Customers" do
+feature "Viewing Customers" do
 
   before do
     visit '/'
     click_link "Customers"
   end
 
-  context "when clicking a Customer name" do
-    #Click Customer name link
+  describe "the main Customer page" do
+    let!(:customer) { FactoryGirl.create(:customer) }
+
+    it "should show a list of all the Customers" do
+      expect(page).to have_content(customer.full_name)
+    end
+  end
+
+  describe "clicking a Customer name" do
+    let!(:customer) { FactoryGirl.create(:customer) }
+    click_link customer.full_name
+
     it "should show the Customer information" do
       #Display Customer factory attributes   
     end
